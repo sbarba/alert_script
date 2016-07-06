25c3cd4ab4de9e99658fb623cbd537b33c1b9970
sbarba <stevenbarba@gmail.com>
2016-07-05 18:07:06 -0700

    - Made it send email even when there are no updates. - Improved comments. - Stripped whitespace from wc command output.

--- new_set_alert.sh
+++ new_set_alert.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
 # TODO
-# Send email even if there are no sets. May turn this off later, but confirmation for now would be good.
 # Change pull to fetch and test.
 
+# Get the set directory and email address from constants.txt. Example content of constants.txt:
+#     /path/to/repo/Star-Wars-LCG-OCTGN/o8g/Sets
+#     user@email.com
 
-# Get the set directory and email address.
 IFS=$'\n' read -d '' -r -a lines < constants.txt
 set_dir=${lines[0]}
 email=${lines[1]}
 
-# Get the number of sets.
-before_no_sets=$(ls $set_dir | wc -l)
+# Get the number of sets. tr command strips whitespace.
+before_no_sets=$(ls $set_dir | wc -l | tr -d '[[:space:]]')
 
 # Fetch
 cd $set_dir
 git pull > /dev/null
 
 # Get the number of sets after the fetch.
-after_no_sets=$(ls $set_dir | wc -l)
+after_no_sets=$(ls $set_dir | wc -l | tr -d '[[:space:]]')
 
-# DEV: Make it think there's a new set.
+# DEV: Uncomment to make it think there's a new set.
 # after_no_sets=$((before_no_sets+1))
 
 # If there are more sets after the fetch email me.
@@ -29,8 +30,10 @@ then
 	new_set=$(ls -tr ${set_dir} | tail -n1)
 	body="
 $((after_no_sets-before_no_sets)) more set(s) after fetch.
-The most recent is ${new_set}.
+Most recent: ${new_set}
 "
-
 	mail -s "New Set! ${new_set}" $email <<< $body
+else
+	mail -s "No new sets" $email <<< "Still ${after_no_sets}."
+
 fi
