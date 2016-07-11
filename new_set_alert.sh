fc8cc048c33ca5dbcdb99a32aa615d8f1351b604
Steve <stevenbarba@gmail.com>
2016-07-11 15:35:03 -0700

    Reformatted.

--- new_set_alert.sh
+++ new_set_alert.sh
@@ -2,12 +2,11 @@
 
 # Get the set directory and email address from constants.txt. Example in README.md.
 constants_path=$(dirname $0)/constants.txt
-
 IFS=$'\n' read -d '' -r -a lines < $constants_path
 set_dir=${lines[0]}
 email=${lines[1]}
 
-# Get the number of sets. tr command strips whitespace.
+# Get the number of sets. tr strips whitespace.
 before_no_sets=$(ls $set_dir | wc -l | tr -d '[[:space:]]')
 
 # Pull
@@ -23,13 +22,12 @@ after_no_sets=$(ls $set_dir | wc -l | tr -d '[[:space:]]')
 # If there are more sets after the pull email me.
 if [ $after_no_sets -gt $before_no_sets ]
 then
-	new_set=$(ls -tr ${set_dir} | tail -n1)
-	body="
+    new_set=$(ls -tr ${set_dir} | tail -n1)
+    body="
 $((after_no_sets-before_no_sets)) more set(s) after pull.
 Most recent: ${new_set}
 "
-	mail -s "New Set! ${new_set}" $email <<< $body
+    mail -s "New Set! ${new_set}" $email <<< $body
 else
-	mail -s "No new sets" $email <<< "Still ${after_no_sets}"
-
+    mail -s "No new sets" $email <<< "Still ${after_no_sets}"
 fi
